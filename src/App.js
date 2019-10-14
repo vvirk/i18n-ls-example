1bbbaed173e8c0de5d90e8666f5c4b57d9c41278
Boxing199 <virkolomeo@gmail.com>
2019-10-14 13:59:18 +0300

    added handleClickOutside

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { withTranslation } from "react-i18next";
-
-//import { LanguagesNav } from './components/LanguagesNav/LanguagesNav';
 
 //styles
 import s from "./styles/app.module.scss";
@@ -10,6 +8,19 @@ export const App = ({ i18n, t }) => {
   const lsLanguage = localStorage.getItem("language");
   const [language, setLanguage] = useState(lsLanguage ? lsLanguage : "ua");
   const [menu, toggleMenu] = useState(false);
+  const node = useRef();
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+  const handleClickOutside = e => {
+    if (node.current.contains(e.target)) {
+      return;
+    }
+    toggleMenu(false);
+  };
   const languageChange = lang => {
     return (
       i18n.changeLanguage(lang),
@@ -18,15 +29,16 @@ export const App = ({ i18n, t }) => {
       toggleMenu(false)
     )
   };
+
   return (
     <div className={s.app}>
       <h1 className={s.title}>{t("title")}</h1>
       <div>{t("description")}</div>
-      <div className={s.wrap}>
+      <div className={s.wrap} ref={node}>
         <nav className={menu ? s.menu : `${s.menu} ${s.close}`}>
           <button
             className={s.menuItem}
-            onClick={() => languageChange("ua") }
+            onClick={() => languageChange("ua")}
           >
             ua
           </button>
